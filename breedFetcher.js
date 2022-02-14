ed30d925210409f2e76b5e8a37bd5b6ad5808d32
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-14 10:07:12 -0500

    created new index.js file and made the code more modular to be able to write some tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require("request");
 
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      const description = data.length === 0 ? "Breed not found" : data[0].description;
+      callback(error, description);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (Array.isArray(data) && data.length === 0) {
-    console.log("Breed not found");
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = {
+  fetchBreedDescription
+};
